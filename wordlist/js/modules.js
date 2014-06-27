9d35dbd0783939c940eb212483706b280f3e5dee
LinguList <mattis.list@posteo.de>
2014-06-27 16:03:21 +0200

    added module loader for easy bundling of packages

---
+++ wordlist/js/modules.js
@@ -0,0 +1,18 @@
+var requirements = [
+  "jquery-1.10.2.js",
+  "jquery-ui-1.10.4.custom.js",
+  "FileSaver.js",
+  "wordlist.js", 
+  "starter.js"
+  ];
+
+for(var i=0;i<requirements.length;i++)
+{
+  var r=requirements[i];
+  var script = document.createElement('script');
+  script.src = 'js/'+r;
+  script.async = false;
+  script.type="text/javascript";
+  document.head.appendChild(script);
+}
+
